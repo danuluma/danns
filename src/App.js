d87f3ebbe392a26a2ba2efac3cbda03c06704145
Dan Uluma <danuluma@gmail.com>
2019-03-10 23:21:30 +0300

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import MemeMaker from "./components/Manual";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
-    return <div className="App">Coming soon</div>;
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <NavBar />
+          <Switch>
+            <Route path="/" render={() => <MemeMaker />} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
   }
 }
 
